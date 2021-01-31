fb09b43c9bc0b2390e7a3f34651d59b83e3549f6
mikhail.kongoyev <mikhail.kongoyev@nedra.digital>
2021-01-31 15:40:01 +0300

    - sudoku generator test

--- src/com/example/sudoku/test/SudokuGeneratorTest.java
+++ src/com/example/sudoku/test/SudokuGeneratorTest.java
@@ -12,23 +12,31 @@ public class SudokuGeneratorTest {
 
     private static BoardFactory boardFactory;
     private static CellHider cellHider;
+    private static SudokuStorage sudokuStorage;
 
     @BeforeClass
     public static void beforeClass() {
         boardFactory = Mockito.mock(BoardFactory.class);
         cellHider = Mockito.mock(CellHider.class);
+        sudokuStorage = new SudokuStorage();
     }
 
     @Test
     public void testThatStorageIsFilledSudoku() {
         int[][] array = {{1, 1, 1}, {1, 1, 1}};
-        Cell[][] hidingArray = {{new Cell(1, true), new Cell(1, true)}, {new Cell(1, true), new Cell(1, true)}};
+        Cell[][] hidingArray = {{new Cell(1, true),
+                                    new Cell(1, true)},
+                                {new Cell(1, true),
+                                    new Cell(1, true)}};
+
         Mockito.when(boardFactory.generateBoard()).thenReturn(array);
+        Mockito.when(boardFactory.getFieldSize()).thenReturn(FieldSize.SMALL);
         Mockito.when(cellHider.makeBoardWithHiddenCells(array)).thenReturn(hidingArray);
-        Sudoku sudoku = new Sudoku(hidingArray, FieldSize.SMALL);
-        SudokuStorage sudokuStorage = new SudokuStorage();
-        sudokuStorage.add(sudoku);
-        Sudoku su = sudokuStorage.giveRequesterSudoku(FieldSize.SMALL, sudoku.getLevel());
-        Assert.assertTrue(sudoku.equals(su));
+
+        SudokuGenerator generator = new SudokuGenerator(boardFactory, cellHider, sudokuStorage);
+        generator.fillStorage();
+
+        Sudoku su = sudokuStorage.giveRequesterSudoku(FieldSize.SMALL, Level.EASY);
+        Assert.assertNotNull(su);
     }
 }
